c94dbd73937c9fef6b6e5e0f79771f7af692e64c
Divyansh Bhardwaj <divyanshb22@gmail.com>
2022-06-20 22:17:10 +0530

    Update TemperatureConverterTest.java

--- src/test/java/io/github/dbc/TemperatureConverterTest.java
+++ src/test/java/io/github/dbc/TemperatureConverterTest.java
@@ -3,38 +3,38 @@ package io.github.dbc;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class TemperatureConverterTest {
 
-    private TemperatureConverter temperatureConverter;
+	private TemperatureConverter temperatureConverter;
 
-    @BeforeEach
-    void setUp() {
-        temperatureConverter = new TemperatureConverter();
-    }
+	@BeforeEach
+	void setUp() {
+		temperatureConverter = new TemperatureConverter();
+	}
 
-    @ParameterizedTest(name = "convertCelsiusToFahrenheit({arguments})")
-    @ValueSource(doubles = {31.0, 20.0, 37.4, 19.6, 51.9})
-    void convertCelsiusToFahrenheit(double temperature) {
-        double convertedTemperature = temperatureConverter.convertCelsiusToFahrenheit(temperature);
-        Assertions.assertEquals(
-                temperature * 1.8 + 32.0,
-                convertedTemperature,
-                0.1,
-                "The converted temperature is not correct!"
-        );
-    }
+	@ParameterizedTest(name = "convertCelsiusToFahrenheit({arguments})")
+	@CsvSource(value = {"31.0, 87.8", "20.0, 68.0", "37.0,98.6", "37.4,99.32", "19.6,67.28"})
+	void convertCelsiusToFahrenheit(double temperature, double expectedValue) {
+		double convertedTemperature = temperatureConverter.convertCelsiusToFahrenheit(temperature);
+		Assertions.assertEquals(
+				expectedValue,
+				convertedTemperature,
+				0.1,
+				"The converted temperature is not correct!"
+		);
+	}
 
-    @ParameterizedTest(name = "convertFahrenheitToCelsius({arguments})")
-    @ValueSource(doubles = {97.4, 32.0, 68.0, -10.0, -20.0})
-    void convertFahrenheitToCelsius(double temperature) {
-        double convertedTemperature = temperatureConverter.convertFahrenheitToCelsius(temperature);
-        Assertions.assertEquals(
-                temperature - 32.0 / 1.8,
-                convertedTemperature,
-                0.1,
-                "The converted temperature is not correct!"
-        );
-    }
+	@ParameterizedTest(name = "convertFahrenheitToCelsius({arguments})")
+	@CsvSource(value = {"97.4, 36.3333333", "32.0,0.0", "68.0,20.0", "-10.0,-23.3333333", "-20.0,-28.8888889"})
+	void convertFahrenheitToCelsius(double temperature, double expectedValue) {
+		double convertedTemperature = temperatureConverter.convertFahrenheitToCelsius(temperature);
+		Assertions.assertEquals(
+				expectedValue,
+				convertedTemperature,
+				0.1,
+				"The converted temperature is not correct!"
+		);
+	}
 }
